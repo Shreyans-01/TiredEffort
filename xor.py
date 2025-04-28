ebbd3a0c0a46c7e062f3d5bd66d42c30e12bf5e6
Shreyans Mehta <55597083+Shreyans-01@users.noreply.github.com>
2025-04-27 20:14:59 -0700

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,46 +1,27 @@
-# xor.py
 import random
 import sys
-
-if len(sys.argv) != 4:
-    print("Usage: python3 xor.py <input_file> <output_file> <mode>")
-    print("mode: encrypt_basic, encrypt_advanced, decrypt_basic, decrypt_advanced")
-    sys.exit(1)
-
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-mode = sys.argv[3]
 
 key = [0] * 1094
 random.seed(42)
 
-def mutate_key(letter):
-    for _ in range(ord(letter)):
-        numj = random.randint(4, 250)
-        for __ in range(numj):
-            key[random.randint(0, 1093)] = random.randint(1, 250)
+with open(sys.argv[1], "rb") as infile:
+    while True:
+        c = sys.stdin.read(1)
+        if c:
+            for i in range(ord(c)):
+                numj = random.randint(4, 250)
+                for j in range(numj):
+                    key[random.randint(0, 1093)] = random.randint(1, 250)
+        else:
+            break
 
-if mode in ['encrypt_basic', 'decrypt_basic']:
-    mutate_key('A')
-elif mode in ['encrypt_advanced', 'decrypt_advanced']:
-    mutate_key('A')
-    mutate_key('B')
-else:
-    print("Invalid mode, must be encrypt_basic, encrypt_advanced, decrypt_basic, or decrypt_advanced")
-    sys.exit(1)
-
-with open(input_file, "rb") as f:
-    data = f.read()
-
-half = len(data) // 2
-
-# Decide what data to operate on
-if mode in ['decrypt_basic']:
-    data = data[:half]
-elif mode in ['decrypt_advanced']:
-    data = data[half:]
-# (for encrypt_basic/advanced we use full data)
-
-with open(output_file, "wb") as f:
-    for i, byte in enumerate(data):
-        f.write(bytes([(byte ^ key[i % 1094]) & 0x7F]))
+    outfile = open(sys.argv[2], "ab")
+    count = 0
+    while True:
+        byte = infile.read(1)
+        if byte:
+            outbyte = (int.from_bytes(byte) ^ key[count % 1094] & 0x7F).to_bytes(1)
+            outfile.write(outbyte)
+            count += 1
+        else:
+            break
