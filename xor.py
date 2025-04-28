9032c6d2b92a96303fe458e2d4726b363efe4a57
Shreyans Mehta <55597083+Shreyans-01@users.noreply.github.com>
2025-04-27 20:09:45 -0700

    Update xor.py

--- xor.py
+++ xor.py
@@ -4,38 +4,43 @@ import sys
 
 if len(sys.argv) != 4:
     print("Usage: python3 xor.py <input_file> <output_file> <mode>")
-    print("mode: basic or advanced")
+    print("mode: encrypt_basic, encrypt_advanced, decrypt_basic, decrypt_advanced")
     sys.exit(1)
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-mode = sys.argv[3]  # 'basic' or 'advanced'
+mode = sys.argv[3]
 
 key = [0] * 1094
 random.seed(42)
 
-# Build the key
-for char in ('A' if mode == 'basic' else 'AB'):
-    for _ in range(ord(char)):
+def mutate_key(letter):
+    for _ in range(ord(letter)):
         numj = random.randint(4, 250)
         for __ in range(numj):
             key[random.randint(0, 1093)] = random.randint(1, 250)
 
-# Open and read in.bin
+if mode in ['encrypt_basic', 'decrypt_basic']:
+    mutate_key('A')
+elif mode in ['encrypt_advanced', 'decrypt_advanced']:
+    mutate_key('A')
+    mutate_key('B')
+else:
+    print("Invalid mode, must be encrypt_basic, encrypt_advanced, decrypt_basic, or decrypt_advanced")
+    sys.exit(1)
+
 with open(input_file, "rb") as f:
     data = f.read()
 
-# Choose correct part
 half = len(data) // 2
-if mode == 'basic':
-    data = data[:half]
-elif mode == 'advanced':
-    data = data[half:]
-else:
-    print("Invalid mode, must be 'basic' or 'advanced'")
-    sys.exit(1)
 
-# Decrypt and write output
+# Decide what data to operate on
+if mode in ['decrypt_basic']:
+    data = data[:half]
+elif mode in ['decrypt_advanced']:
+    data = data[half:]
+# (for encrypt_basic/advanced we use full data)
+
 with open(output_file, "wb") as f:
     for i, byte in enumerate(data):
         f.write(bytes([(byte ^ key[i % 1094]) & 0x7F]))
